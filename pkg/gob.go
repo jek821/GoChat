6ceb0ffd3d5bcc90f0cdc3805990528719e99f34
Jemanuel <jacobkriss2001@gmail.com>
2025-12-21 16:44:49 -0500

    updated comments explaining gob registers

--- pkg/gob.go
+++ pkg/gob.go
@@ -3,5 +3,8 @@ package pkg
 import "encoding/gob"
 
 func Init() { 
-	gob.Register(&TestData{})
+	// Transmission Struct holds Data{} interface
+	// Data constructors return pointers so we must register pointer types
+	// with gob so that it knows how to encrypt and decrypt transmissions.
+	gob.Register(&TestData{}) 
 }
